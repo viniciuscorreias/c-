1307b22dfcc3ee5c73d79ba640ad5a076565819d
viniciuscorreias <vinicius.ascorreia@gmail.com>
2026-03-12 16:13:08 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,12 +3,25 @@
 using namespace std;
 int main() {
   
+    int b;
+     b=60; 
+    int a;
+    cin >> a;
 
-    int a, b;
-    cin >> a >> b;
-    cout << a / b << "\n";
-    
 
+    int minuto; 
+    minuto = 0;
+    minuto = a / b;
+    int segundo;
+    segundo = 0;
+    segundo = a % b;
+    int hora;
+    hora = 0;
+    hora = minuto / b;
+    int minuto_final;
+    minuto_final = minuto - (hora * b);
+
+    cout << hora << ":" <<  minuto_final << ":" << segundo << "\n";
 
     return 0;
 }
